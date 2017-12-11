a5a5826854860a6d6ce9f670efb009a7992c373f
nealszentmiklosy <nszent@gmail.com>
2017-12-11 14:30:02 -0500

    finished programming game

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -9,7 +9,31 @@ function player(height, age) {
 }
 
 let players = {};
-players.John = new player(172, 24);
-players.Mark = new player(170, 17);
+let msg;
+let winner = {
+  winner: "",
+  score: 0
+};
+players.John = new player(6, 4);
+players.Mark = new player(6, 4);
+players.Steve = new player(7, 4);
 
-console.log(players.John);
+for (let key in players) {
+  if (players.hasOwnProperty(key)) {
+    let s = players[key];
+    s.score = result(s.height, s.age);
+    if (s.score > winner.score) {
+      winner.winner = key;
+      winner.score = s.score;
+      msg = "winner is ";
+    } else if (s.score == winner.score) {
+      winner.winner += " and " + key;
+      msg = "winners are ";
+    }
+  }
+}
+
+// What if there are two winners? How would you change the text to say "the winners are"?
+// Hint: maybe use an array for winners.winner
+//
+console.log("The " + msg + winner.winner + " with a score of " + winner.score);
